60f6a5d5cb3f306d13bdddb265c9807769dce51a
MattisDm <mda.new@mail.ru>
2023-08-26 17:36:01 +0300

    initial commit

---
+++ Lesson 1/Task 1/HomeWork1.1/HomeWork1.1.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char** argv) {
+    std::string s;
+    std::ifstream fin("in.txt");
+    std::ofstream fout("out.txt");
+
+    if (!fin.is_open()) {
+        std::cout << "File not opened";
+        return 1;
+    }
+
+    if (!fout.is_open()) {
+        std::cout << "File not opened";
+        return 2;
+    }
+
+    int n = 0;
+    int m = 0;
+
+    fin >> n;
+    int tmpN = 0;
+    fin >> tmpN;
+    int* arrN = new int[n];
+    for (int i = 0; i < (n - 1); i++) {
+        fin >> arrN[i];
+    }
+    arrN[n - 1] = tmpN;
+
+    fin >> m;
+    int* arrM = new int[m];
+    for (int i = 1; i < m; i++) {
+        fin >> arrM[i];
+    }
+    fin >> arrM[0];
+
+    fout << m << std::endl;
+    for (int i = 0; i < m; i++) {
+        fout << arrM[i] << " ";
+    }
+    fout << std::endl;
+
+    fout << n << std::endl;
+    for (int i = 0; i < n; i++) {
+        fout << arrN[i] << " ";
+    }
+    fout << std::endl;
+
+    delete[] arrN;
+    delete[] arrM;
+
+    fin.close();
+    fout.close();
+
+    return 0;
+}
---
+++ Lesson 2/Task 1/HomeWork2.1/HomeWork2.1.cpp
@@ -0,0 +1,67 @@
+﻿
+#include <iostream>
+#include <string>
+
+enum Months {
+    Stop,
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+};
+
+std::string displayMonth2String(Months &month) {
+    switch (month) {
+    case Stop:
+        return "До свидания";
+    case January:
+        return "Январь";
+    case February:
+        return "Февраль";
+    case March:
+        return "Март";
+    case April:
+        return "Апрель";
+    case May:
+        return "Май";
+    case June:
+        return "Июнь";
+    case July:
+        return "Июль";
+    case August:
+        return "Август";
+    case September:
+        return "Сентябрь";
+    case October:
+        return "Октябрь";
+    case November:
+        return "Ноябрь";
+    case December:
+        return "Декабрь";
+    default:
+        break;
+    }
+    return "Не правильный номер!";
+}
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Russian");
+    Months month = January;
+    while (month != Stop) {
+        int monthInt;
+        std::cout << "Введите номер месяца: ";
+        std::cin >> monthInt;
+        Months month = static_cast<Months>(monthInt);
+        std::cout << displayMonth2String(month) << std::endl;
+    }
+    return 0;
+}
